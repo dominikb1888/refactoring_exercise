37fc2950bcb6e94821d36a768f2cd374663f539f
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-01-24 16:33:34 +0100

    removed all staticmethods and create a proper class, change testsuite to accomodate

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -2,24 +2,26 @@ import csv
 
 
 class FundingRaised:
-    def filter_csv(csv_data, options):
+    def __init__(self, path="../startup_funding.csv"):
+        self.path = path
+        with open(self.path, "rt") as csvfile:
+            file_data = csv.reader(csvfile, delimiter=",", quotechar='"')
+            self.headers = next(file_data)
+            self.data = [dict(zip(self.headers, i)) for i in file_data]
+
+    def _filter_csv(data, options):
         if options:
             key = list(options.keys())[0]
-            fltrd = [row for row in csv_data if row[key] == options[key]]
+            fltrd = [row for row in data if row[key] == options[key]]
             del options[key]
 
-        return FundingRaised.filter_csv(fltrd, options) if options else fltrd
+        return FundingRaised._filter_csv(fltrd, options) if options else fltrd
 
-    def where(options={}):
-        with open("../startup_funding.csv", "rt") as csvfile:
-            file_data = csv.reader(csvfile, delimiter=",", quotechar='"')
-            headers = next(file_data)
-            csv_data = [dict(zip(headers, i)) for i in file_data]
+    def where(self, options={}):
+        return FundingRaised._filter_csv(self.data, options)
 
-        return FundingRaised.filter_csv(csv_data, options)
-
-    def find_by(options):
-        result = FundingRaised.where(options)
+    def find_by(self, options):
+        result = FundingRaised.where(self, options=options)
         return result[0]
 
 
