ea104c3712c0749dbf7098d494416c0284ce98e1
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-01-25 13:20:15 +0100

    added DictReader and cleaned up naming

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -5,17 +5,19 @@ class FundingRaised:
     def __init__(self, path="../startup_funding.csv"):
         self.path = path
         with open(self.path, "rt") as csvfile:
-            file_data = csv.reader(csvfile, delimiter=",", quotechar='"')
-            self.headers = next(file_data)
-            self.data = [dict(zip(self.headers, i)) for i in file_data]
+            self.data = [row for row in csv.DictReader(csvfile)]
 
     def _filter_csv(data, options):
         if options:
+            # We are recursing through the keys of the options passed in by the user
+            # deleting the option after processing it to filter the data
+            # If the key yielded a result it is appended to the result
+            # Recursion stops if options.keys() are empty and result is passed back.
             key = list(options.keys())[0]
-            fltrd = [row for row in data if row[key] == options[key]]
+            result = [row for row in data if row[key] == options[key]]
             del options[key]
 
-        return FundingRaised._filter_csv(fltrd, options) if options else fltrd
+        return FundingRaised._filter_csv(result, options) if options else result
 
     def where(self, options={}):
         return FundingRaised._filter_csv(self.data, options)
